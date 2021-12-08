e2f5702717268e643e1bb86109c3561467338a4d
Robert Davis <robdave22@yahoo.com>
2021-12-08 16:50:43 -0500

    Server.js calls the other files

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ app.use(express.static('public'))
 
 //Rollbar goes here
 
-//app.get filepaths go here
+app.get('/', (req, res) => {
+    //rollbar.info('Someone visited our site')
+
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.css'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.js'))
+})
 
 //app.get from api goes here
 
